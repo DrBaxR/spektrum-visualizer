88d7137b8a72909293d1973a2be0e653e78cade5
Alexandru Florea <alexflorea16000@gmail.com>
2022-05-06 14:22:12 +0300

    Filtering by metrics

--- src/components/Tree/TreeNode.tsx
+++ src/components/Tree/TreeNode.tsx
@@ -3,6 +3,8 @@ import { ExportUnit } from "../../model/export-unit";
 import './TreeNode.css';
 import { Search } from "../Search";
 import { TreeHeader } from "./TreeHeader";
+import { MetricFilter, MetricFilterFormSchema } from "../MetricFilter";
+import * as FilterFunctions from './constants/filter-functions';
 
 interface Props {
   node: ExportUnit;
@@ -19,6 +21,9 @@ export const TreeNode: React.FC<Props> = ({ node, expanded, onIdentifierClick })
     return map;
   });
   const [filteredChildren, setFilteredChildren] = useState(node.children);
+
+  let nameFilterFunction: (node: ExportUnit) => boolean = () => true;
+  let metricFilterFunction: (node: ExportUnit) => boolean = () => true;
 
   const isChildExpanded = (child: ExportUnit): boolean => {
     return !!childrenNodesExpanded.get(child.identifier);
@@ -46,14 +51,67 @@ export const TreeNode: React.FC<Props> = ({ node, expanded, onIdentifierClick })
     );
   };
 
-  const handleFilterValueChange = (newValue: string) => {
+  const filterChildren = () => {
     const filteredChildren = node.children.filter(c => {
-      const splitId = c.identifier.split("->");
-      const shortId = splitId[splitId.length - 1];
-      return shortId.toLowerCase().includes(newValue.toLowerCase());
+      return nameFilterFunction(c) && metricFilterFunction(c);
     })
 
     setFilteredChildren(filteredChildren);
+  }
+
+  const handleFilterValueChange = (newValue: string) => {
+    nameFilterFunction = node => {
+      const splitId = node.identifier.split("->");
+      const shortId = splitId[splitId.length - 1];
+      return shortId.toLowerCase().includes(newValue.toLowerCase())
+    };
+
+    filterChildren();
+  }
+
+  const setMetricFilterFunction = (change: MetricFilterFormSchema): ((node: ExportUnit) => boolean) => {
+    const amount = Number(change.amount);
+
+    switch (change.metric) {
+      case 'test': {
+        // testAmount
+        switch (change.operation) {
+          case 'greater':
+            return (node: ExportUnit) => FilterFunctions.tag(node, amount);
+          case 'less':
+            return (node: ExportUnit) => FilterFunctions.tal(node, amount);
+          case 'greater-eq':
+            return (node: ExportUnit) => FilterFunctions.tage(node, amount);
+          case 'less-eq':
+            return (node: ExportUnit) => FilterFunctions.tale(node, amount);
+          default:
+            return () => true
+        }
+      }
+      case 'coverage': {
+        // coverage
+        switch (change.operation) {
+          case 'greater':
+            return (node: ExportUnit) => FilterFunctions.cg(node, amount);
+          case 'less':
+            return (node: ExportUnit) => FilterFunctions.cl(node, amount);
+          case 'greater-eq':
+            return (node: ExportUnit) => FilterFunctions.cge(node, amount);
+          case 'less-eq':
+            return (node: ExportUnit) => FilterFunctions.cle(node, amount);
+          default:
+            return () => true;
+        }
+      }
+      default:
+        return () => true;
+    }
+  }
+
+  const handleMetricFilterChange = (change: MetricFilterFormSchema): void => {
+    metricFilterFunction = setMetricFilterFunction(change);
+
+    filterChildren();
   }
 
   const hasChildren = (): boolean => {
@@ -65,7 +123,11 @@ export const TreeNode: React.FC<Props> = ({ node, expanded, onIdentifierClick })
       <div className="header">
         <TreeHeader expanded={expanded} onIdentifierClicked={() => onIdentifierClick(node.identifier)} node={node} />
 
-        {hasChildren() && expanded && <Search onValueChanged={handleFilterValueChange} />}
+        {hasChildren() && expanded && (
+        <div className="filters">
+          <Search onValueChanged={handleFilterValueChange} />
+          <MetricFilter onChange={handleMetricFilterChange}/>
+        </div>)}
       </div>
 
       {expanded && getNodeChildren()}
---
+++ src/components/Tree/constants/filter-functions.ts
@@ -0,0 +1,33 @@
+import { ExportUnit } from "../../../model/export-unit";
+
+export const tag = (unit: ExportUnit, amount: number): boolean => {
+  return unit.testAmount * 100 > amount
+}
+
+export const tal = (unit: ExportUnit, amount: number): boolean => {
+  return unit.testAmount * 100 < amount
+}
+
+export const tage = (unit: ExportUnit, amount: number): boolean => {
+  return unit.testAmount * 100 >= amount
+}
+
+export const tale = (unit: ExportUnit, amount: number): boolean => {
+  return unit.testAmount * 100 <= amount
+}
+
+export const cg = (unit: ExportUnit, amount: number): boolean => {
+  return unit.coverage * 100 > amount
+}
+
+export const cl = (unit: ExportUnit, amount: number): boolean => {
+  return unit.coverage * 100 < amount
+}
+
+export const cge = (unit: ExportUnit, amount: number): boolean => {
+  return unit.coverage * 100 >= amount
+}
+
+export const cle = (unit: ExportUnit, amount: number): boolean => {
+  return unit.coverage * 100 <= amount
+}
